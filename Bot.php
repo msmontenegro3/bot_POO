11651bbbdba7c0ccfd89108770aa8f7c337d3699
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-06-16 22:26:15 -0500

    prueba

--- Bot.php
+++ Bot.php
@@ -3,34 +3,20 @@
 //token y métodos
 class Bot{
 
-    public string $metodo;
-    private string $url;
+    function sendMessage($chatID, $messaggio, $token,&$k = ''){
+        $url = "https://api.telegram.org/" . $token . "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID;
     
-
-    public function APICall(string $metodo){
-        $url = 'https://api.telegram.org/bot'. '5334366629:AAEFOK9CnKLe3e2xStyI_QnFOai8jAMb0c4' .$metodo;
-        echo $url;
-        $ch = curl_init();
-        $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
+        if(isset($k)) {
+            $url = $url."&reply_markup=".$k; 
+            }
+    
+    
+            $url = $url."&text=" . urlencode($messaggio);
+            $ch = curl_init();
+            $optArray = array(
+                    CURLOPT_URL => $url,
+                    CURLOPT_RETURNTRANSFER => true
         );
-
-        curl_setopt_array($ch, $optArray);
-        $result = curl_exec($ch);
-        curl_close($ch);
-    }
-
-    public function sendMessage($chatID, $messaggio, &$k = ''){
-        $metodo = "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID . "&text=" . urldecode($messaggio);        
-        $url = 'https://api.telegram.org/bot'. '5334366629:AAEFOK9CnKLe3e2xStyI_QnFOai8jAMb0c4' .$metodo;
-        echo $url;
-        $ch = curl_init();
-        $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-        );
-
         curl_setopt_array($ch, $optArray);
         $result = curl_exec($ch);
         curl_close($ch);
