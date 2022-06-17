b33100faf3ac81216f0435a1171880073ce87539
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-06-16 23:31:54 -0500

    prueba

--- conexion.php
+++ conexion.php
@@ -3,10 +3,11 @@
 /*class Conexion{*/
 
     function conectar(){
-        $user="root";
-        $pass="";
-        $server="localhost";
-        $db= "oop_bot";
+        $user="
+        pelon196_mirta";
+        $pass="Mirta2022";
+        $server="mysql1007.mochahost.com";
+        $db= "pelon196_mirta_utpl";
 
         $con = new mysqli($server, $user, $db);
         $con->set_charset("utf8");
