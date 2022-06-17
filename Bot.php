a671c84907a9705ed16c0798d114aa9c1da8ee8b
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-06-16 22:24:15 -0500

    prueba

--- Bot.php
+++ Bot.php
@@ -23,8 +23,16 @@ class Bot{
 
     public function sendMessage($chatID, $messaggio, &$k = ''){
         $metodo = "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID . "&text=" . urldecode($messaggio);        
-        echo "llega aquí";
-        return Bot::APICall($metodo);
+        $url = 'https://api.telegram.org/bot'. '5334366629:AAEFOK9CnKLe3e2xStyI_QnFOai8jAMb0c4' .$metodo;
+        echo $url;
+        $ch = curl_init();
+        $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+        );
 
+        curl_setopt_array($ch, $optArray);
+        $result = curl_exec($ch);
+        curl_close($ch);
     }
 }
