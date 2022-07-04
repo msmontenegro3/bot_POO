b50a830c69b3eb334c5ef9bf08615486c8fb8aa3
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-07-03 20:33:41 -0500

    clases

--- main2.php
+++ main2.php
@@ -1,5 +1,54 @@
 <?php
 
+class Webhook{
+
+    private $message;
+    private $text;
+
+    public function __construct($update)
+    {
+        $this->update = $update;
+        $this->message = $update['message'];
+        $this->text = $message["text"];
+    }
+
+    public function reciver()
+    {
+        $chatID = 1073553770;
+        $messaggio = 'HOLA WEY';
+        $token= 'bot5334366629:AAEFOK9CnKLe3e2xStyI_QnFOai8jAMb0c4';
+
+        if(isset($this->text) && $this->text == '/start' ){
+    
+            $this->sendMessage($chatID, $messaggio, $token);
+        } 
+    }
+
+    public function sendMessage($chatID, $messaggio, $token,&$k = ''){
+        $url = "https://api.telegram.org/" . $token . "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID;
+    
+        if(isset($k)) {
+            $url = $url."&reply_markup=".$k; 
+            }
+    
+    
+            $url = $url."&text=" . urlencode($messaggio);
+            $ch = curl_init();
+            $optArray = array(
+                    CURLOPT_URL => $url,
+                    CURLOPT_RETURNTRANSFER => true
+        );
+        curl_setopt_array($ch, $optArray);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+
+}
+
+$wb->reciver();
+$wb = new Webhook(json_decode(file_get_contents("php://input"),true));
+return;
+
 $update = json_decode(file_get_contents("php://input"),true);
 $message = $update['message'];
 $text = $message["text"];
