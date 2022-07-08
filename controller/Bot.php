75437bed5a69a3c2fe68064348f85f7e03da1eff
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-07-07 21:21:30 -0500

    pruebas

--- controller/Bot.php
+++ controller/Bot.php
@@ -21,6 +21,23 @@ class Bot{
     //ENVÍA MENSAJES Y TECLADO
     function sendMessage($chatID, $messaggio, $token,&$k = ''){
 
+        $keyboard = [
+            "inline_keyboard" => [
+                [
+                    [
+                        "text" => "✅",
+                        "callback_data" => "presentarPreguntas(x)"
+                    ],
+                    [
+                        "text" => "❌",
+                        "callback_data" => "recall_start()"
+                    ]
+                ]
+            ]
+        ];
+        $k=json_encode($keyboard);
+
+        
         file_put_contents('llegaASendMessage', $k);
         $url = "https://api.telegram.org/" . $token . "/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=" . $chatID;
         /* $url = "https://api.telegram.org/" . $token . "/sendMessage?&chat_id=" . $chatID; */
