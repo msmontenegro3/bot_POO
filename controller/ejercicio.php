6f9612754f68d384688b8fa58989c8de655e6e19
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-07-08 13:18:32 -0500

    pruebas

--- controller/ejercicio.php
+++ controller/ejercicio.php
@@ -98,7 +98,7 @@ class Ejercicio
         $numero_preguntas = count($arreglo_preguntas);
         
         if ($contador < $numero_preguntas) {
-            $imprimir = 'Pregunta ' . $array_param_preguntas[1] + 1 . ': ' .$arreglo_preguntas[$contador]['pregunta'];
+            $imprimir = ($contador + 1) . ': ' .$arreglo_preguntas[$contador]['pregunta'];
 
             $this->respuesta_correcta = $arreglo_preguntas[$contador]['id_respuesta_correcta'];
            // $this->feedback_pregunta = $arreglo_preguntas[$contador]['feedback'];
